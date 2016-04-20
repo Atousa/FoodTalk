8a218c78ea9af9d9ac976bcb41a5fea0ca3032ad
Eric Hong <ericdhong@gmail.com>
2016-04-20 13:56:33 -0700

    Assigned the proper VC.swift file to each VC in storyboard

---
+++ FoodTalk/FavoriteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteListViewController.h
+//  FoodTalk
+//
+//  Created by Eric Hong on 4/20/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteListViewController : UIViewController
+
+@end
---
+++ FoodTalk/FavoriteListViewController.m
@@ -0,0 +1,29 @@
+//
+//  FavoriteListViewController.m
+//  FoodTalk
+//
+//  Created by Eric Hong on 4/20/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import "FavoriteListViewController.h"
+
+@interface FavoriteListViewController ()
+
+@end
+
+@implementation FavoriteListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
