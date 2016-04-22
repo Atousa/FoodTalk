cbc280cc922df972b5b1628d0f71a6a1dbcd9c87
Eric Hong <ericdhong@gmail.com>
2016-04-22 13:55:46 -0700

    Re-added bridge header file and imported the necessary Yelp .h files

---
+++ FoodTalk/FavoriteListViewController.h
@@ -0,0 +1,36 @@
+//
+//  FavoriteListViewController.h
+//  FoodTalk
+//
+//  Created by Eric Hong on 4/22/16.
+//  Copyright © 2016 EricDHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YLPBusiness.h"
+#import "YLPCategory.h"
+#import "YLPClient.h"
+#import "YLPClient+Business.h"
+#import "YLPClient+PhoneSearch.h"
+#import "YLPClient+Search.h"
+#import "YLPClientPrivate.h"
+#import "YLPCommonPrivate.h"
+#import "YLPCoordinate.h"
+#import "YLPDeal.h"
+#import "YLPDealOption.h"
+#import "YLPGeoBoundingBox.h"
+#import "YLPGeoCoordinate.h"
+#import "YLPGiftCertificate.h"
+#import "YLPGiftCertificateOption.h"
+#import "YLPLocation.h"
+#import "YLPPhoneSearch.h"
+#import "YLPRegion.h"
+#import "YLPResponsePrivate.h"
+#import "YLPReview.h"
+#import "YLPSearch.h"
+#import "YLPSortType.h"
+#import "YLPUser.h"
+
+@interface FavoriteListViewController : UIViewController
+
+@end
