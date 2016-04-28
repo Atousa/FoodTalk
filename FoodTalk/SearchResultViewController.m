f60f6a490dd0c66ef2a69ea3afd4ae164016ec88
Eric Hong <ericdhong@gmail.com>
2016-04-28 15:21:14 -0700

    Added deselect row method

--- FoodTalk/SearchResultViewController.m
+++ FoodTalk/SearchResultViewController.m
@@ -142,6 +142,10 @@
     return cell;
 }
 
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
